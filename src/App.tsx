0a7b4e39f05cd38fd7c3ada6d3c9f3d3927a5f9e
Higor Moreira <higorldmoreira@gmail.com>
2026-04-06 19:28:10 -0300

    feat: adiciona componente DefaultInput e atualiza o formulário no App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Container } from './components/Container'
 import { Heading } from './components/Heading'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
+
 
 import './style/theme.css'
 import './style/Global.css'
-import { CountDown } from './components/CountDown'
 
 
 
@@ -22,6 +24,24 @@ export default function App() {
       </Container>
       <Container>
         <CountDown/>
+      </Container>
+      <Container>
+        <form className='form' action="">
+          <div className='formRow'>
+            <DefaultInput type='text'/>
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos.</p>
+            <p> 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button type='submit'>Start</button>
+          </div>
+        </form>
+        
       </Container>
   </>
 }
